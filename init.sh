f6d369ff4cb86cae916ff0c23ba46bc45a563f70
TangJie <tangjie@codemao.cn>
2023-04-27 17:06:11 +0800

    create

---
+++ init.sh
@@ -0,0 +1,17 @@
+#rm -rf .git
+## domains
+git clone https://github.com/LilMosey/customer-domain.git domains/customer-domain
+git clone https://github.com/LilMosey/order-domain.git domains/order-domain
+
+## apps
+git clone https://github.com/LilMosey/order-apps.git order-apps
+
+## starters
+git clone https://github.com/LilMosey/order-starters.git order-starters
+
+## webs
+git clone https://github.com/LilMosey/order-webs.git order-webs
+
+## infrastructures
+git clone https://github.com/LilMosey/customer-infrastructure.git infrastructures/customer-infrastructure
+git clone https://github.com/LilMosey/order-infrastructure.git infrastructures/order-infrastructure
